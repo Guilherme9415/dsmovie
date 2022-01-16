9734089f2f1e59e37489929b672144623ff57059
Guilherme <guilhermehssantos79@gmail.com>
2022-01-16 13:47:51 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -17,7 +17,7 @@ function MovieCard({ movie } : Props) {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score}/>
         <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
         </Link>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -20,7 +20,7 @@ function Listing() {
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then((response) => {
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then((response) => {
       const data = response.data as MoviePage;
       setPage(data);
     });
