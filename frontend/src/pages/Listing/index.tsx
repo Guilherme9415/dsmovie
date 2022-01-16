6c1a93efbbada265de62db53ba6f27f7cece1156
Guilherme <guilhermehssantos79@gmail.com>
2022-01-16 00:59:49 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,25 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
-  axios.get(`${BASE_URL}/movies`).then(response => {
-  console.log(response.data);
-  });
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+  }, []);
 
   return (
     <>
       <Pagination />
-
+      <p>{pageNumber}</p>
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
@@ -37,3 +44,6 @@ function Listing() {
 }
 
 export default Listing;
+function userEffect(arg0: any) {
+  throw new Error("Function not implemented.");
+}
