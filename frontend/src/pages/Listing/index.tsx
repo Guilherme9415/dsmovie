0bb1656f210e5c1570878cb6f22b74f03bccb4d7
Guilherme <guilhermehssantos79@gmail.com>
2022-01-16 00:43:20 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+  axios.get(`${BASE_URL}/movies`).then(response => {
+  console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
